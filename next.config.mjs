98a1b2ecee39a1c7230c9c30ae7abc44e740c3fd
mir6u <discordbackupacc1234@gmail.com>
2024-05-21 15:02:42 +0300

    ss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.guns.lol",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
